c5bbb2b61e49ab535977d52a50c56779efa885a2
Mariana da Silva Lara <marislaradev@outlook.com>
2024-03-25 19:06:02 -0300

    feat: create Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer className="w-full h-16 bg-red-700 text-white text-lg flex items-center justify-center fixed bottom-0">
+            Feito com 💖 por Mariana da Silva Lara &copy; 2024
+        </footer>
+    )
+}
+
+export default Footer
