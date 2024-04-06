6f4fa1af000431cf52be50cc3ec353446dc8e7f1
Mariana da Silva Lara <marislaradev@outlook.com>
2024-04-06 11:51:35 -0300

    style: adjust Tailwind CSS for QuantitySelector and add it to PizzaItem component

--- src/components/PizzaItem/index.jsx
+++ src/components/PizzaItem/index.jsx
@@ -1,3 +1,5 @@
+import QuantitySelector from "../QuantitySelector";
+
 function PizzaItem({ name, ingredients, price }) {
   const formattedPrice = price.toLocaleString("pt-BR", {
     style: "currency",
@@ -8,12 +10,10 @@ function PizzaItem({ name, ingredients, price }) {
     <section className="p-6 bg-white rounded-xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all transform duration-500">
       <h2 className="text-2xl font-bold text-gray-700">{name}</h2>
       <p className="text-sm mt-2 text-gray-700">{ingredients}</p>
-      <p className="block text-xl font-semibold text-gray-700 cursor-auto">
+      <p className="block text-xl font-semibold text-gray-700 cursor-auto mt-1">
         {formattedPrice}
       </p>
-      <button className="mt-4 text-lg block font-semibold py-2 px-6  hover:text-white bg-molho_de_tomate rounded-lg shadow hover:shadow-md transition duration-300">
-        Adicionar ao carrinho
-      </button>
+     <QuantitySelector />
     </section>
   );
 }
--- src/components/QuantitySelector/index.jsx
+++ src/components/QuantitySelector/index.jsx
@@ -4,7 +4,7 @@ function QuantitySelector() {
   const [quantity, setQuantity] = useState(0);
 
   const handleDecrease = () => {
-    if (quantity > 1) {
+    if (quantity > 0) {
       setQuantity(quantity - 1);
     }
   };
@@ -14,25 +14,25 @@ function QuantitySelector() {
   };
 
   return (
-    <div className="flex items-center">
+    <div className="flex flex-row h-10 w-32 rounded-lg relative bg-transparent mt-4">
       <button
         onClick={handleDecrease}
-        className="bg-gray-200 px-3 py-1 rounded-1-md focus:outline-none"
+        className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
       >
-        -
+        <span class="m-auto text-2xl font-thin">−</span>
       </button>
       <input
         type="number"
         min="0"
         value={quantity}
         readOnly
-        className="w-16 px-2 py-1 border border-gray-300 text-center focus:outline-none"
+        className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700"
       />
       <button
         onClick={handleIncrease}
-        className="bg-gray-200 px-3 py-1 rounded-1-md focus:outline-none"
+        className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
       >
-        +
+        <span class="m-auto text-2xl font-thin">+</span>
       </button>
     </div>
   );
