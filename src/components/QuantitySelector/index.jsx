41e4aada593a0d52d37e76a000ae32d613f47b40
Mariana da Silva Lara <marislaradev@outlook.com>
2024-04-06 12:07:17 -0300

    fix: replace class with className in component QuantitySelector

--- src/components/QuantitySelector/index.jsx
+++ src/components/QuantitySelector/index.jsx
@@ -19,7 +19,7 @@ function QuantitySelector() {
         onClick={handleDecrease}
         className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
       >
-        <span class="m-auto text-2xl font-thin">−</span>
+        <span className="m-auto text-2xl font-thin">−</span>
       </button>
       <input
         type="number"
@@ -32,7 +32,7 @@ function QuantitySelector() {
         onClick={handleIncrease}
         className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
       >
-        <span class="m-auto text-2xl font-thin">+</span>
+        <span className="m-auto text-2xl font-thin">+</span>
       </button>
     </div>
   );
